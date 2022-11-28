ca2290456c0a0aaa4a15f6b4a166b6e1fba97fb0
Chouchene Mohamed <mo.chouchene@gmail.com>
2022-11-28 18:46:11 +0100

    Instanciation du projet SprignBoot

---
+++ src/main/java/com/example/katabanque/KatabanqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.katabanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatabanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatabanqueApplication.class, args);
+	}
+
+}
